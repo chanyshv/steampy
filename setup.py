75699af02843a6788d8f4c35a5c7e8a697a0c26a
Damir Chanyshev <hairygeek@yandex.com>
2021-06-19 09:30:15 +0500

    update setup.py

--- setup.py
+++ setup.py
@@ -7,15 +7,14 @@ if not sys.version_info[0] == 3 and sys.version_info[1] < 5:
 version = '0.74'
 
 setup(
-    name='steampy',
+    name='steampy-proxy',
     packages=['steampy', 'test', 'examples', ],
     version=version,
     description='A Steam lib for trade automation',
     author='Michał Bukowski',
     author_email='gigibukson@gmail.com',
     license='MIT',
-    url='https://github.com/bukson/steampy',
-    download_url='https://github.com/bukson/steampy/tarball/' + version,
+    url='https://github.com/hairygeek/steampy',
     keywords=['steam', 'trade', ],
     classifiers=[],
     install_requires=[
